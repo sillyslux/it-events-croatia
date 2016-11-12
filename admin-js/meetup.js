8fb131c43bd496df466cee78ead4069fcfa1b3f6
sillyslux <sillyslux@net.hr>
2016-11-12 18:21:12 +0100

    initial commit

--- admin-js/meetup.js
+++
@@ -1,9 +0,0 @@
-// modules/NavLink.js
-import React from 'react'
-import { Link } from 'react-router'
-
-export default React.createClass({
-  render() {
-    return <h1>Meetup form here</h1>
-  }
-})
